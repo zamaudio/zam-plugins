a2a549152a098d5efd06858ccaeb9bec1f15f275
Damien Zammit <damien@zamaudio.com>
2014-04-13 02:21:52 +1000

    Added artwork
    Signed-off-by: Damien Zammit <damien@zamaudio.com>

--- plugins/ZamComp/ZamCompArtwork.hpp
+++ plugins/ZamComp/ZamCompArtwork.hpp
@@ -10,6 +10,16 @@ namespace ZamCompArtwork
     const unsigned int knobWidth    = 42;
     const unsigned int knobHeight   = 42;
 
+    extern const char* ledredData;
+    const unsigned int ledredDataSize = 484;
+    const unsigned int ledredWidth    = 11;
+    const unsigned int ledredHeight   = 11;
+
+    extern const char* ledyellowData;
+    const unsigned int ledyellowDataSize = 484;
+    const unsigned int ledyellowWidth    = 11;
+    const unsigned int ledyellowHeight   = 11;
+
     extern const char* zamcompData;
     const unsigned int zamcompDataSize = 256800;
     const unsigned int zamcompWidth    = 800;
