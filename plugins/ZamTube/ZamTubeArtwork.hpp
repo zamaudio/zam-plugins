bb6dff095c64f4f00289bf2aeb174cf532822a4c
Damien Zammit <damien@zamaudio.com>
2014-04-16 03:23:09 +1000

    Added ZamTube artwork
    Signed-off-by: Damien Zammit <damien@zamaudio.com>

--- plugins/ZamTube/ZamTubeArtwork.hpp
+++ plugins/ZamTube/ZamTubeArtwork.hpp
@@ -1,35 +1,25 @@
 /* (Auto-generated binary data file). */
 
-#ifndef BINARY_WOBBLEJUICEARTWORK_HPP
-#define BINARY_WOBBLEJUICEARTWORK_HPP
+#ifndef BINARY_ZAMTUBEARTWORK_HPP
+#define BINARY_ZAMTUBEARTWORK_HPP
 
 namespace ZamTubeArtwork
 {
-    extern const char* aboutData;
-    const unsigned int aboutDataSize = 180000;
-    const unsigned int aboutWidth    = 300;
-    const unsigned int aboutHeight   = 200;
-
-    extern const char* aboutButtonHoverData;
-    const unsigned int aboutButtonHoverDataSize = 5888;
-    const unsigned int aboutButtonHoverWidth    = 92;
-    const unsigned int aboutButtonHoverHeight   = 16;
-
-    extern const char* aboutButtonNormalData;
-    const unsigned int aboutButtonNormalDataSize = 5888;
-    const unsigned int aboutButtonNormalWidth    = 92;
-    const unsigned int aboutButtonNormalHeight   = 16;
-
-    extern const char* backgroundData;
-    const unsigned int backgroundDataSize = 450000;
-    const unsigned int backgroundWidth    = 500;
-    const unsigned int backgroundHeight   = 300;
-
     extern const char* knobData;
-    const unsigned int knobDataSize = 12544;
-    const unsigned int knobWidth    = 56;
-    const unsigned int knobHeight   = 56;
+    const unsigned int knobDataSize = 7056;
+    const unsigned int knobWidth    = 42;
+    const unsigned int knobHeight   = 42;
+
+    extern const char* notchData;
+    const unsigned int notchDataSize = 2016;
+    const unsigned int notchWidth    = 21;
+    const unsigned int notchHeight   = 24;
+
+    extern const char* zamtubeData;
+    const unsigned int zamtubeDataSize = 423360;
+    const unsigned int zamtubeWidth    = 448;
+    const unsigned int zamtubeHeight   = 315;
 }
 
-#endif // BINARY_WOBBLEJUICEARTWORK_HPP
+#endif // BINARY_ZAMTUBEARTWORK_HPP
 
