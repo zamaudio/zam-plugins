ddfeec2128ee4133e7ae98bd9f75a1e66953bbe9
Damien Zammit <damien@zamaudio.com>
2015-11-10 20:43:24 +1100

    Fixed title on headers
    Signed-off-by: Damien Zammit <damien@zamaudio.com>

--- plugins/ZamDelay/ZamDelayUI.hpp
+++ plugins/ZamDelay/ZamDelayUI.hpp
@@ -1,6 +1,6 @@
 /*
- * ZamDelay stereo compressor
- * Copyright (C) 2014  Damien Zammit <damien@zamaudio.com>
+ * ZamDelay mono delay effect
+ * Copyright (C) 2015  Damien Zammit <damien@zamaudio.com>
  *
  * This program is free software; you can redistribute it and/or
  * modify it under the terms of the GNU General Public License as
